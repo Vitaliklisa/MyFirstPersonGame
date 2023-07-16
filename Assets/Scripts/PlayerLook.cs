ab916bc3d4f18d8ccd739575f99f669aa478a024
vitaliikhomenko <vhomenko119@gmail.com>
2023-07-16 15:51:46 -0400

    Remove mouse cursor

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerLook : MonoBehaviour
 {
@@ -15,11 +16,15 @@ public class PlayerLook : MonoBehaviour
     float currentVelosityX;
     float currentVelosityY;
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     public void ProcessLook(Vector2 input)
     {
         xRot += Input.GetAxis("Mouse X") * sensivity;
         yRot += Input.GetAxis("Mouse Y") * sensivity;
-        yRot = Mathf.Clamp(yRot, -90, 90);
+        yRot = Mathf.Clamp(yRot, -89f, 89f);
 
         xRotCurrent = Mathf.SmoothDamp(xRotCurrent, xRot, ref currentVelosityX, smoothTime);
         yRotCurrent = Mathf.SmoothDamp(yRotCurrent, yRot, ref currentVelosityY, smoothTime);

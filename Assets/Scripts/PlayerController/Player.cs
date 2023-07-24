d801bb7bfdecfa86fed2b25be0cb445655bfa1f7
vitaliikhomenko <vhomenko119@gmail.com>
2023-07-23 20:25:00 -0400

    Add first person flying

--- Assets/Scripts/PlayerController/Player.cs
+++ Assets/Scripts/PlayerController/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float mouseSensitivity = 3f;
-    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float walkingSpeed = 5f;
+    [SerializeField] float flyingSpeed = 10f;
     [SerializeField] float climbingSpeed = 2f;
     [SerializeField] float mass = 1f;
     [SerializeField] float acceleration = 20f;
@@ -27,6 +28,14 @@ public class Player : MonoBehaviour
 
     internal float movementSpeedMultiplier;
 
+    public State state;
+
+    public enum State
+    {
+        Walking,
+        Flying
+    }
+
     CharacterController controller;
     internal Vector3 velocity;
     Vector2 look;
@@ -38,13 +47,14 @@ public class Player : MonoBehaviour
     PlayerInput playerInput;
     InputAction MoveAction;
     InputAction lookAction;
-
+    InputAction flyUpDownAction;
     void Awake()
     {
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
         MoveAction = playerInput.actions["move"];
         lookAction = playerInput.actions["look"];
+        flyUpDownAction = playerInput.actions["flyUpDown"];
     }
 
     void Start()
@@ -64,11 +74,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        UpdateGround();
-        UpdateGravity();
-        UpdateMovement();
-        UpdateLook();
-        CheckBounds();
+        movementSpeedMultiplier = 1f;
+        switch (state)
+        {
+            case State.Walking:
+                UpdateGround();
+                UpdateGravity();
+                UpdateMovement();
+                UpdateLook();
+                CheckBounds();
+                break;
+            case State.Flying:
+                UpdateMovementFlying();
+                UpdateLook();
+                break;
+        }
     }
 
     void CheckBounds()
@@ -94,29 +114,43 @@ public class Player : MonoBehaviour
         velocity.y = controller.isGrounded ? -1f : velocity.y + gravity.y;
     }
 
-    Vector3 GetMovementInput()
+    Vector3 GetMovementInput(float speed, bool horizontal = true)
     {
         var moveInput = MoveAction.ReadValue<Vector2>();
+        var flyUpDownInput = flyUpDownAction.ReadValue<float>();
         var input = new Vector3();
+        var referenceTransform = horizontal ? transform : cameraTransform;
         input += transform.forward * moveInput.y;
         input += transform.right * moveInput.x;
+        if (!horizontal)
+        {
+            input += transform.up * flyUpDownInput;
+        }
         input = Vector3.ClampMagnitude(input, 1f);
-        input *= movementSpeed * movementSpeedMultiplier;
+        input *= speed * movementSpeedMultiplier;
         return input;
     }
 
     void UpdateMovement()
     {
-        movementSpeedMultiplier = 1f;
         OnBeforeMove?.Invoke();
 
-        var input = GetMovementInput();
+        var input = GetMovementInput(walkingSpeed);
 
         var factor = acceleration * Time.deltaTime;
         velocity.x = Mathf.Lerp(velocity.x, input.x, factor);
         velocity.z = Mathf.Lerp(velocity.z, input.z, factor);
 
-        controller.Move(velocity* Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void UpdateMovementFlying()
+    {
+        var input = GetMovementInput(flyingSpeed, false);
+
+        var factor = acceleration * Time.deltaTime;
+        velocity = Vector3.Lerp(velocity, input, factor);
+        controller.Move(velocity * Time.deltaTime);
     }
 
     void UpdateLook()
@@ -131,4 +165,8 @@ public class Player : MonoBehaviour
         transform.localRotation = Quaternion.Euler(0, look.x, 0);
     }
 
+    void OnToggleFlying()
+    {
+        state = state == State.Flying ? State.Walking : State.Flying;
+    }
 }
